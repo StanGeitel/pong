70c664c818b06eed6478757a4694892265c8f9eb
StanGeitel <stan@geitel.nl>
2018-03-06 17:15:27 +0100

    controller

---
+++ Controller/Controller/i2c.h
@@ -0,0 +1,16 @@
+/*
+ * i2c.h
+ *
+ * Created: 06/03/2018 12:58:17
+ *  Author: stan
+ */ 
+
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+
+
+
+#endif /* I2C_H_ */
---
+++ Controller/Controller/main.c
@@ -0,0 +1,18 @@
+/*
+ * Controller.c
+ *
+ * Created: 05/03/2018 20:07:34
+ * Author : stan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

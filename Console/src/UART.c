fcc2cb96d1f666e46863a8400e1d89aaf8f80a02
sven1123 <35920547+sven1123@users.noreply.github.com>
2018-04-03 14:37:11 +0200

    a

---
+++ Console/src/UART.c
@@ -0,0 +1,16 @@
+//UART 0
+
+#define PINSEL0		0x4002C000	//8.5.1 Pin function select reg.0
+
+
+
+#define PCONP
+#define PCLKSEL0
+#define U0RBR		0x4000c000 //14.4.1
+#define U0IER		0x4000C004 // UART interrupt enable register
+
+
+
+
+
+

68dcb9ad49b0882eb1f386cfc9e9ecf0be7cb3ff
sven1123 <35920547+sven1123@users.noreply.github.com>
2018-04-10 20:45:06 +0200

    UART

--- Console_C/src/Console_C.c
+++ Console_C/src/Console_C.c
@@ -19,21 +19,35 @@
 #include "UART.h"
 #include "stdutils.h"
 // TODO: insert other definitions and declarations here
+//#define PINSEL3		(* (unsigned int* )(0x4002C00C))
+//#define CLKSRCSEL   (* (unsigned int* )(0x400FC10C))
+//#define CLKOUTCFG	(* (unsigned int* )(0x400FC1C8))
 
 int main(void) {
-	int ch;
+	unsigned char ch;
 
+	//PINSEL3 |= (0x1 << 22);
+	//CLKOUTCFG |= (0x0 << 0);
+	//CLKOUTCFG |= (0x1 << 8);
     printf("Start\n");
     UART_Init();
+    //uart_TxChar(0x11);
+
+    //ch = uart_RxChar();
+    //printf(ch);
 
     // Force the counter to be placed into memory
     volatile static int i = 0 ;
     // Enter an infinite loop, just incrementing a counter
     while(1) {
+    	//uart_TxChar(0xD7);
 
     	ch = uart_RxChar();
-    	printf(ch);
-    	printf("- \n");
+    	printf("%x \n", ch);
+    	//ch++;
+
+    	//uart_TxChar(ch);
+    	for(int x = 0; x < 100000; x++);
         i++ ;
     }
     return 0 ;
--- Console_C/src/UART.c
+++ Console_C/src/UART.c
@@ -1,79 +1,72 @@
 
 #define PINSEL0		(* (unsigned int* )(0x4002C000))	//8.5.1 Pin function select register 0
+#define PINSEL1		(* (unsigned int* )(0x4002C004))
 #define PCLKSEL0 	(* (unsigned int* )(0x400FC1A8))	//4.7.3 Peripheral Clock Selection register 0
 
-#define LCR0		(* (unsigned int *)(0x4000C00C))	//14.4.7 UART Line Control Register
-#define LSR0		(* (unsigned int *)(0x4000C014))	//14.4.8 UART Line Status Register
-#define RBR0		(* (unsigned int *)(0x4000C000))	//14.4.1 UART Receiver Buffer Register
-#define FCR0		(* (unsigned int *)(0x4000C008))	//14.4.6 UART FIFO Control Register
-#define DLL0		(* (unsigned int *)(0x4000C000))	//14.4.3 UART Divisor Latch LSB Register
-#define DLM0		(* (unsigned int *)(0x4000C004))	//14.4.3 UART Divisor Latch MSB Register
-#define IER0		(* (unsigned int *)(0x4000C004))	//14.4.4 UART Interrupt Enable Register
+#define THR1		(* (unsigned int *)(0x40010000))	//15.4.2 Transmit Holding Register
+#define LCR1		(* (unsigned int *)(0x4001000C))	//15.4.7 UART Line Control Register
+#define LSR1		(* (unsigned int *)(0x40010014))	//15.4.10 UART Line Status Register
+#define RBR1		(* (unsigned int *)(0x40010000))	//15.4.1 UART Receiver Buffer Register
+#define FCR1		(* (unsigned int *)(0x4001C008))	//15.4.6 UART FIFO Control Register
 
-#define clockCoreFreq (12000000UL)
+#define DLL1		(* (unsigned int *)(0x40010000))	//15.4.3 UART Divisor Latch LSB Register
+#define DLM1		(* (unsigned int *)(0x40010004))	//15.4.3 UART Divisor Latch MSB Register
+
+#define FDR1		(* (unsigned int *)(0x40010028))	//15.4.16 UART Fractional Divider Register
+
+#define IER1		(* (unsigned int *)(0x4001C004))	//15.4.4 UART Interrupt Enable Register
+
+#define clockCoreFreq (1100000UL)
+#define pClock (1000000)
+
+#define baudrate 9600
 
 #include "UART.h"
 #include "stdutils.h"
 
 void UART_Init(void)
 {
-	uint32_t UartPclk, Pclk, RegValue;
+	uint32_t UartPclk, Pclk, Fdiv;
 
-	PINSEL0 &= ~(0xF << 4);
-	PINSEL0 |=  (0x1 << 6);		// pin P0.3 als RXD0
-
-	IER0 |= (0x1<<0);			//Enable the RDA interrupts.
-
-	FCR0 &= ~(0x7<<0);
-	FCR0 |= (0x3<<0); 			// Enable FIFO and reset Rx FIFO buffers
-
-	LCR0 &= ~(0xFF<<0);
-	LCR0 |= (0x9B<<0); 			// 8bit data, 1Stop bit, Even parity, Enable access to Divisor Latches -- 10011011
+	PINSEL0 &= ~(0x3 << 30);
+	PINSEL0 |=  (0x1 << 30);		//p0.15 als TXD1
+	PINSEL1 &= ~(0x3 << 0);
+	PINSEL1 |=  (0x1 << 0);			//p0.16 als RXD1
 
 
-	/** Baud Rate Calculation :
-	   PCLKSELx registers contains the PCLK info for all the clock dependent peripherals.
-	   Bit6,Bit7 contains the Uart Clock(ie.UART_PCLK) information.
-	   The UART_PCLK and the actual Peripheral Clock(PCLK) is calculated as below.
-	   (Refer data sheet for more info)
+	FCR1 &= ~(0x7<<0);
+	FCR1 |= (0x7<<0); 			// Enable FIFO and reset Rx FIFO buffers
 
-	   UART_PCLK    PCLK
-		 0x00       SystemFreq/4
-		 0x01       SystemFreq
-		 0x02       SystemFreq/2
-		 0x03       SystemFreq/8
-	 **/
+	LCR1 &= ~(0xFF<<0);
+	LCR1 |= (0x9B<<0); 			// 8bit data, 1Stop bit, Even parity, Enable access to Divisor Latches -- 10011011
+	//-------------------------------------------------------
 
-	UartPclk = (PCLKSEL0 & (0x03<<6));
+	//Fdiv = ( clockCoreFreq / 16 ) / baudrate ;    // Set baud rate
 
-	switch( UartPclk )
-	{
-		  case 0x00:
-			Pclk = clockCoreFreq/4;
-			break;
-		  case 0x01:
-			Pclk = clockCoreFreq;
-			break;
-		  case 0x02:
-			Pclk = clockCoreFreq/2;
-			break;
-		  case 0x03:
-			Pclk = clockCoreFreq/8;
-			break;
-	}
+	//DLM1 = Fdiv / 256;
+	//DLL1 = Fdiv % 256;
 
-	RegValue = ( Pclk / (16 * 9600 )); //9600 baud rate
-	DLL0 =  RegValue & 0xFF;
-	DLM0 = (RegValue >> 0x08) & 0xFF;
+	Fdiv = ( pClock / (16 * 9600 ));
+	DLL1 =  Fdiv & 0xFF;
+	DLM1 = (Fdiv >> 0x08) & 0xFF;
 
-	LCR0 &= ~(0x1<<7);  // Clear DLAB after setting DLL,DLM
+	//-------------------------------------------------------
+	LCR1 &= ~(0x1<<7);  	// Clear DLAB after setting DLL,DLM
+
+	IER1 |= (0x1<<0);			//Enable the RDA interrupts.
 }
 
-int uart_RxChar()
+void uart_TxChar(unsigned char ch)
 {
-	int ch;
-    while( !(LSR0 & (1 << 7)) && !(LSR0 & (1 << 0))); //wait until any data arrives in Rx FIFO
-    ch = RBR0;               	// Read received data
+    while( !(LSR1 & ( 1 << 5)) ); // Wait for Previous transmission
+    THR1=ch;                                  // Load the data to be transmitted
+}
+
+unsigned char uart_RxChar()
+{
+	unsigned char ch;
+    while( !(LSR1 & (1 << 0)) ); 			//wait until any data arrives in Rx FIFO
+    ch = RBR1 & (0xFF << 0); 				// Read received data
     return ch;
 }
 

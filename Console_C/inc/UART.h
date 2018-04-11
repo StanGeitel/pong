1e2fca71d5d8766ccf1dd7ac7dba20eff4001dbd
sven1123 <35920547+sven1123@users.noreply.github.com>
2018-04-11 16:18:55 +0200

    UART

--- Console_C/inc/UART.h
+++ Console_C/inc/UART.h
@@ -8,8 +8,33 @@
 #ifndef UART_H_
 #define UART_H_
 
+
+#define PINSEL0		(* (unsigned int* )(0x4002C000))	//8.5.1 Pin function select register 0
+#define PINSEL1		(* (unsigned int* )(0x4002C004))
+#define PCLKSEL0 	(* (unsigned int* )(0x400FC1A8))	//4.7.3 Peripheral Clock Selection register 0
+
+#define THR1		(* (unsigned int *)(0x40010000))	//15.4.2 Transmit Holding Register
+#define LCR1		(* (unsigned int *)(0x4001000C))	//15.4.7 UART Line Control Register
+#define LSR1		(* (unsigned int *)(0x40010014))	//15.4.10 UART Line Status Register
+#define RBR1		(* (unsigned int *)(0x40010000))	//15.4.1 UART Receiver Buffer Register
+#define FCR1		(* (unsigned int *)(0x4001C008))	//15.4.6 UART FIFO Control Register
+
+#define DLL1		(* (unsigned int *)(0x40010000))	//15.4.3 UART Divisor Latch LSB Register
+#define DLM1		(* (unsigned int *)(0x40010004))	//15.4.3 UART Divisor Latch MSB Register
+
+#define FDR1		(* (unsigned int *)(0x40010028))	//15.4.16 UART Fractional Divider Register
+
+#define IER1		(* (unsigned int *)(0x40010004))	//15.4.4 UART Interrupt Enable Register
+#define IIR1		(* (unsigned int *)(0x40010008))	//15.4.5 UART Interrupt Identification Register
+
+
+#define clockCoreFreq (3000000UL)
+#define pClock (1000000)
+
+#define baudrate 4800
+#endif /* UART_H_ */
+
 void UART_Init(void);
 void uart_TxChar(unsigned char ch);
 unsigned char uart_RxChar();
-
-#endif /* UART_H_ */
+void UART1_IRQHandler(void);
--- Console_C/src/UART.c
+++ Console_C/src/UART.c
@@ -1,32 +1,24 @@
 
-#define PINSEL0		(* (unsigned int* )(0x4002C000))	//8.5.1 Pin function select register 0
-#define PINSEL1		(* (unsigned int* )(0x4002C004))
-#define PCLKSEL0 	(* (unsigned int* )(0x400FC1A8))	//4.7.3 Peripheral Clock Selection register 0
-
-#define THR1		(* (unsigned int *)(0x40010000))	//15.4.2 Transmit Holding Register
-#define LCR1		(* (unsigned int *)(0x4001000C))	//15.4.7 UART Line Control Register
-#define LSR1		(* (unsigned int *)(0x40010014))	//15.4.10 UART Line Status Register
-#define RBR1		(* (unsigned int *)(0x40010000))	//15.4.1 UART Receiver Buffer Register
-#define FCR1		(* (unsigned int *)(0x4001C008))	//15.4.6 UART FIFO Control Register
-
-#define DLL1		(* (unsigned int *)(0x40010000))	//15.4.3 UART Divisor Latch LSB Register
-#define DLM1		(* (unsigned int *)(0x40010004))	//15.4.3 UART Divisor Latch MSB Register
-
-#define FDR1		(* (unsigned int *)(0x40010028))	//15.4.16 UART Fractional Divider Register
-
-#define IER1		(* (unsigned int *)(0x4001C004))	//15.4.4 UART Interrupt Enable Register
-
-#define clockCoreFreq (3000000UL)
-#define pClock (1000000)
-
-#define baudrate 9600
-
+#include <stdio.h>
 #include "UART.h"
 #include "stdutils.h"
 
+void UART1_IRQHandler(void){
+	uint32_t tmp;
+	unsigned char ch;
+	tmp |= (IIR1 & 0xE) ;
+
+	// Receive Data Available
+	if ((tmp == 0x4) || (tmp == 0xC))
+	{
+		ch = uart_RxChar();
+		printf("%x \n", ch);
+	}
+}
+
 void UART_Init(void)
 {
-	uint32_t UartPclk, Pclk, Fdiv;
+	uint32_t Fdiv;
 
 	PINSEL0 &= ~(0x3 << 30);
 	PINSEL0 |=  (0x1 << 30);		//p0.15 als TXD1
@@ -46,14 +38,15 @@ void UART_Init(void)
 	//DLM1 = Fdiv / 256;
 	//DLL1 = Fdiv % 256;
 
-	Fdiv = ( pClock / (16 * 4800 ));
+	Fdiv = ( pClock / (16 * baudrate ));
 	DLL1 =  Fdiv & 0xFF;
 	DLM1 = (Fdiv >> 0x08) & 0xFF;
 
 	//-------------------------------------------------------
 	LCR1 &= ~(0x1<<7);  	// Clear DLAB after setting DLL,DLM
 
-	IER1 |= (0x1<<0);			//Enable the RDA interrupts.
+	IER1 |= (0x5<<0);			//Enable the RDA interrupts.
+	//IIR1 |= ()
 }
 
 void uart_TxChar(unsigned char ch)

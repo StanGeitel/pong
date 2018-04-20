023018a11a381b4170d08ed2e216054a6f5bc359
sven1123 <35920547+sven1123@users.noreply.github.com>
2018-04-20 12:00:19 +0200

    new console

---
+++ console/console/main.c
@@ -0,0 +1,21 @@
+/*
+ * console.c
+ *
+ * Created: 18-4-2018 16:28:55
+ * Author : Sven
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}

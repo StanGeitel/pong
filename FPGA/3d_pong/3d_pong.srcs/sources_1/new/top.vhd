cce4638e281b7cd888d8283f674fc22306ffa5c3
LucahvanGeloven <37582480+LucahvanGeloven@users.noreply.github.com>
2018-05-15 16:54:43 +0200

    3dpongfpga

--- FPGA/3d_pong/3d_pong.srcs/sources_1/new/top.vhd
+++ FPGA/3d_pong/3d_pong.srcs/sources_1/new/top.vhd
@@ -28,19 +28,33 @@ component spi port(
     rx_data : out STD_LOGIC_VECTOR(16 downto 0));
 end component;
 
-component vga is port( 	
-    clkVGA : in STD_LOGIC;
-    red_in, green_in, blue_in : in STD_LOGIC_VECTOR(3 downto 0);
-    h_count, v_count : out STD_LOGIC_VECTOR(9 downto 0);
-    hsync, vsync : out STD_LOGIC;
-    red, green, blue : out STD_LOGIC_VECTOR(3 downto 0));
-end component;
+component vga
+   port(clk_25MHz : in STD_LOGIC;
+       red : out STD_LOGIC_VECTOR(3 downto 0);
+       green : out STD_LOGIC_VECTOR(3 downto 0);
+       blue : out STD_LOGIC_VECTOR(3 downto 0);
+       hsync, vsync : out  STD_LOGIC;
+       red_in : in STD_LOGIC_VECTOR(3 downto 0);
+       green_in : in STD_LOGIC_VECTOR(3 downto 0);
+       blue_in : in STD_LOGIC_VECTOR(3 downto 0);
+       hcount_out : out STD_LOGIC_VECTOR(9 downto 0);
+       vcount_out : out STD_LOGIC_VECTOR(9 downto 0));
+       end component;
+       
+signal hcount_out, vcount_out: std_logic_vector(9 downto 0);
 
-component image is port( 
-    clk25MHz : in STD_LOGIC;
-    h_count, v_count : in STD_LOGIC_VECTOR(9 downto 0);
-    red, green, blue : out STD_LOGIC_VECTOR(3 downto 0));
-end component;
+component image 
+    port(clk_25MHz : in STD_LOGIC;
+         red : out STD_LOGIC_VECTOR(3 downto 0);
+         green : out STD_LOGIC_VECTOR(3 downto 0);
+         blue : out STD_LOGIC_VECTOR(3 downto 0);
+         hcount : in std_logic_vector (9 downto 0);
+         vcount : in std_logic_vector (9 downto 0));
+    end component;
+    
+signal red_out : STD_LOGIC_VECTOR(3 downto 0);
+signal green_out : STD_LOGIC_VECTOR(3 downto 0);
+signal blue_out : STD_LOGIC_VECTOR(3 downto 0);
 
 signal tmpedge : STD_LOGIC;
 signal tmpclkVGA : STD_LOGIC;
@@ -65,24 +79,24 @@ spi1 : spi port map(
     rx_data => frame);
     
 vga1 : vga port map(
-    clkVGA => tmpclkVGA,
-    red_in => tmpred,
-    green_in => tmpgreen,
-    blue_in => tmpblue,
-    h_count => tmphcount,
-    v_count => tmpvcount,
-    hsync => hsync,
-    vsync => vsync,
-    red => red,
-    green => green,
-    blue => blue);
+    tmpclkVGA,
+    red,
+    green,
+    blue,
+    hsync,
+    vsync,
+    red_out,
+    green_out,
+    blue_out,
+    hcount_out,
+    vcount_out);
     
 image1 : image port map(
-    clk25MHz => tmpclkVGA,
-    h_count => tmphcount,
-    v_count => tmpvcount,
-    red => tmpred,
-    green => tmpgreen, 
-    blue => tmpblue);
+    tmpclkVGA,
+    red_out,
+    green_out,
+    blue_out,
+    hcount_out,
+    vcount_out);
 
 end Behavioral;
